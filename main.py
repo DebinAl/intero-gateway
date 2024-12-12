c0f285c9e9545fec4f327bd02705c158899a8da8
wiratana <andikawiratana@gmail.com>
2024-12-12 10:41:57 +0800

    fix bug : failed to add id into token

--- main.py
+++ main.py
@@ -318,7 +318,7 @@ async def login_for_access_token(form_data) -> Token:
         )
     access_token_expires = timedelta(minutes=float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")))
     access_token = create_access_token(
-        data={"id": user.id, "sub": user.username, "role": user.role}, expires_delta=access_token_expires
+        data={"id": str(user._id), "sub": user.username, "role": user.role}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
 
--- schema/user.py
+++ schema/user.py
@@ -1,6 +1,7 @@
 from mongoengine import *
 
 class User(Document):
+    _id = ObjectIdField()
     username = StringField(required=True)
     email = StringField(required=True)
     hashed_password = StringField(required=True)
